a1f770f160050c70887b23fa87bd65a4fbfb24b4
LuanSantos00 <luancosmefilho@gmail.com>
2023-07-11 20:50:27 -0300

    iniciando estudos do refit

---
+++ ViaCepApi_Refit/Controllers/ViaCepController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViaCepApi_Refit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViaCepController : BaseController
+    {
+        public ViaCepController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpGet("{cep}")]
+        public async Task<IActionResult> GetCep(string cep)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ViaCepApi_Refit/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ViaCepApi_Refit
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
